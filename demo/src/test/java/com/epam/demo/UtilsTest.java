b7332ce0213baa4baab3483d64d494b486de5cef
Vladislav Samorukov <smrk.vld@gmail.com>
2023-03-22 02:54:44 +0300

    added tests for demo-utils

---
+++ demo/src/test/java/com/epam/demo/UtilsTest.java
@@ -0,0 +1,29 @@
+package com.epam.demo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+    @Test
+    void returnsTrueWhenAllPositive() {
+        assertTrue(Utils.isAllPositiveNumbers(List.of("1", "2", "3")));
+    }
+
+    @Test
+    void returnsFalseWhenZero() {
+        assertFalse(Utils.isAllPositiveNumbers(List.of("0", "1", "2")));
+    }
+
+    @Test
+    void returnsFalseWhenNegative() {
+        assertFalse(Utils.isAllPositiveNumbers(List.of("1", "3", "-1")));
+    }
+
+    @Test
+    void throwsExceptionWhenNaN() {
+        assertThrows(NumberFormatException.class, () -> Utils.isAllPositiveNumbers(List.of("1", "o", "32")));
+    }
+}
