cac7191aeb25ecc2bf5fa98e58944c43db569bc7
Vladislav Samorukov <smrk.vld@gmail.com>
2023-03-22 03:08:52 +0300

    added tests for demo-utils

--- demo/src/test/java/com/epam/demo/UtilsTest.java
+++ demo/src/test/java/com/epam/demo/UtilsTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UtilsTest {
+class UtilsTest {
     @Test
     void returnsTrueWhenAllPositive() {
         assertTrue(Utils.isAllPositiveNumbers(List.of("1", "2", "3")));
